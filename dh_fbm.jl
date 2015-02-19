9f1ab0a99f85a096212f79cd2333895bea4a974c
Howon Lee <howon@howonlee.com>
2015-02-18 17:30:34 -0800

    readme setup

--- dh_fbm.jl
+++ dh_fbm.jl
@@ -1,5 +1,5 @@
 using Brownian
 using NPZ
 
-p = FBM(0:1.0:1000000, 0.000000001)
+p = FBM(0:1.0:100000, 0.000001)
 npzwrite("quick_fbm.npy", rand(p))
---
+++ readme.md
@@ -0,0 +1,4 @@
+Visibility graphs
+===
+
+Not what the CV people use them for. Instead, investigate graph statistical properties of FBM
