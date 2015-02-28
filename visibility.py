355233452d45b8ebd1117fc864060bde954330fc
Howon Lee <howon@howonlee.com>
2015-02-27 16:45:02 -0800

    for great justice

--- visibility.py
+++ visibility.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as npr
+import networkx as nx
 
 def brownian(length, loc=0, scale=1):
     #1d brownian
     arr = npr.normal(loc, scale, size=length)
     return np.cumsum(arr, axis=-1)
 
+def calc_vis_matrix(arr):
+    vis_mat = np.zeros(arr.shape[0], arr.shape[0]) #duplicate
+    for x in the array:
+        for y also in the array:
+            vis_mat[x,y] = whether we can see
+    return vis_mat
+
 if __name__ == "__main__":
-    plt.plot(brownian(1000))
-    plt.show()
+    sample = brownian(1000)
 
